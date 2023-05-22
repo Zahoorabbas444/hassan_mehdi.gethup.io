efe3a544e998ea367ee58037e9f34df8bcccc31e
Zahoorabbas444 <132945816+Zahoorabbas444@users.noreply.github.com>
2023-05-22 09:30:23 +0500

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,8 @@
+function showAlert() {
+    alert("Message has been send to admin office!");
+}
+function clearField() {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+}
